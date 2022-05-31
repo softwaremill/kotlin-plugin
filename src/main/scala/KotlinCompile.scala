c8429b9a0d4167893d3c3192566c65a336094a60
Allan Brighton <Allan.Brighton@t-online.de>
2022-05-31 22:33:51 +0200

    Trying out kotlin-1.7.0-RC for use with kotlin scripting for esw sequences

--- src/main/scala/KotlinCompile.scala
+++ src/main/scala/KotlinCompile.scala
@@ -158,7 +158,7 @@ case class KotlinStub(s: TaskStreams, kref: KotlinReflection) {
       parser.getMethod("parseCommandLineArguments", classOf[java.util.List[java.lang.String]], commonToolArguments,
         java.lang.Boolean.TYPE)
     import collection.JavaConverters._
-    parserMethod.invoke(null, options.asJava, args, false)
+    parserMethod.invoke(null, options.asJava, args, null)
   }
 
   def compilerArgs = {
